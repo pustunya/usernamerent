928bb2f98d963b87d64c915953c8c17511fa0c64
pustunya <dvsafronoff@gmail.com>
2025-08-15 16:51:53 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,150 @@
+let title = document.querySelector(".username__title")
+
+// let flag = false
+
+
+// function changeTitle() {
+//     if (flag == false) {
+
+
+//         title.innerText = "win"
+//         title.style.backgroundColor = "blue"
+
+//         flag = true
+//     } else{
+//           title.innerText = "ассортимент username"
+//         title.style.backgroundColor = "white"
+        
+// flag = false
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("салам малейкум")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//peremenya
+let yaYa = 90
+
+console.log(yaYa)
+
+yaYa = "makan"
+
+console.log(yaYa)
+
+let переменная = 2
+console.log(переменная)
+
+const red = 6.643
+console.log(red)
